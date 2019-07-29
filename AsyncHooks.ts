1258fa5524c14f0854ea3efea2b74d07a25e41c4
Scott Vance <scott.vance@layerlogicinc.com>
2019-07-29 13:51:01 -0400

    -Copied useAsync to AsyncHooks

---
+++ AsyncHooks.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect, DependencyList, useCallback } from "react";
+
+export function useAsync<T,D>(defaultValue:D,asyncCallback:()=>Promise<T>,deps:DependencyList):T|D
+{
+    const [value,setValue]=useState<T|D>(defaultValue);
+    const cb=useCallback(asyncCallback,deps);
+
+    useEffect(()=>{
+        let active=true;
+        const doCall=async ()=>{
+            try{
+                const r=await cb();
+                if(active){
+                    setValue(r);
+                }
+            }catch(ex){
+                console.error('useAsync callback error',ex);
+            }
+        }
+        doCall();
+        return ()=>{
+            active=false;
+        }
+    },[cb])
+
+    return value;
+}
