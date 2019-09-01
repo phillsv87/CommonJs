cb389f7d41e6dee19cbc748c405cbd242245c176
Phillip Vance <phillipvance@Phillips-MBP.cinci.rr.com>
2019-09-01 13:08:10 -0400

    -Added loadOrDefault

--- ObjStore.ts
+++ ObjStore.ts
@@ -16,6 +16,15 @@ class ObjStore
         }
     }
 
+    loadOrDefault<T>(key:string,defaultValue:T):T{
+        var r=window.localStorage.getItem(this.prefix+'::'+key);
+        if(r){
+            return JSON.parse(r) as T;
+        }else{
+            return defaultValue;
+        }
+    }
+
     save<T>(key:string, value:T):void{
         window.localStorage.setItem(this.prefix+'::'+key,JSON.stringify(value));
     }
