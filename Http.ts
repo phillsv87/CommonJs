a7de3dbba0af7c111d08cce42a9c1cd6365efc2d
Scott Vance <logic@newbaselogic.com>
2019-10-23 10:16:04 -0400

    - Added ui functions that catch and log errors

--- Http.ts
+++ Http.ts
@@ -1,4 +1,5 @@
 import http from 'axios';
+import Log from './Log';
 
 export const simpleAuthHeaderParam:string='SaToken';
 
@@ -7,9 +8,9 @@ export const bearerAuthHeaderParam:string='Authorization';
 
 export default class Http
 {
-    _baseUrl:string;
-    _authToken:string|null;
-    _authHeaderParam:string|null;
+    private _baseUrl:string;
+    private _authToken:string|null;
+    private _authHeaderParam:string|null;
 
 
     constructor(baseUrl:string)
@@ -19,17 +20,23 @@ export default class Http
         this._authHeaderParam=null;
     }
 
-    setBaseUrl=(baseUrl:string)=>{
+    setBaseUrl(baseUrl:string)
+    {
         this._baseUrl=baseUrl;
     }
 
-    getBaseUrl=()=>this._baseUrl;
+    getBaseUrl()
+    {
+        return this._baseUrl;
+    }
 
-    getAsync=(path:string,data:any=null):Promise<any>=>{
+    getAsync<T>(path:string,data:any=null):Promise<T>
+    {
         return this.callAsync('GET',path,data);
     }
 
-    getSingleAsync=async (path:string,data:any=null):Promise<any>=>{
+    async getSingleAsync<T>(path:string,data:any=null):Promise<T>
+    {
         const r=await this.callAsync('GET',path,data);
         if(Array.isArray(r)){
             return r[0];
@@ -38,28 +45,30 @@ export default class Http
         }
     }
 
-    postAsync=(path:string,data:any=null):Promise<any>=>{
+    postAsync<T>(path:string,data:any=null):Promise<T>
+    {
         return this.callAsync('POST',path,data);
     }
 
-    putAsync=(path:string,data:any=null):Promise<any>=>{
+    putAsync<T>(path:string,data:any=null):Promise<T>
+    {
         return this.callAsync('PUT',path,data);
     }
 
-    deleteAsync=(path:string,data:any=null):Promise<any>=>{
+    deleteAsync<T>(path:string,data:any=null):Promise<T>
+    {
         return this.callAsync('DELETE',path,data);
     }
 
-
-
-    postFormAsync=(path:string,formData:any)=>
+    postFormAsync<T>(path:string,formData:any):Promise<T>
     {
         return this.callAsync('POST',path,formData,(r:any)=>{
             r.headers['Content-Type']='multipart/form-data';
         });
     }
 
-    callAsync=async (method:string,path:string,data:any,configRequest:any=null):Promise<any>=>{
+    async callAsync(method:string,path:string,data:any,configRequest:any=null):Promise<any>
+    {
 
         if(path.indexOf('http:')===-1 && path.indexOf('https:')===-1){
             path=this._baseUrl+path;
@@ -95,15 +104,58 @@ export default class Http
         }
     }
 
-    setAuthToken=(token:string|null, authHeaderParam:string=simpleAuthHeaderParam)=>
+    setAuthToken(token:string|null, authHeaderParam:string=simpleAuthHeaderParam)
     {
         this._authToken=token;
         this._authHeaderParam=authHeaderParam;
     }
 
-    setBearerAuthToken=(token:string|null)=>
+    setBearerAuthToken(token:string|null)
     {
         this._authToken=token?'bearer '+token:null;
         this._authHeaderParam=bearerAuthHeaderParam;
+    }
+
+
+
+
+    uiGetAsync<T>(path:string,data:any=null):Promise<T|null>
+    {
+        return this.uiContext<T>(()=>(this.getAsync(path,data) as Promise<T>));
+    }
+
+    uiGetSingleAsync<T>(path:string,data:any=null):Promise<T|null>
+    {
+        return this.uiContext<T>(()=>(this.getSingleAsync(path,data) as Promise<T>));
+    }
+
+    uiPostAsync<T>(path:string,data:any=null):Promise<T|null>
+    {
+        return this.uiContext<T>(()=>(this.postAsync(path,data) as Promise<T>));
+    }
+
+    uiPutAsync<T>(path:string,data:any=null):Promise<T|null>
+    {
+        return this.uiContext<T>(()=>(this.putAsync(path,data) as Promise<T>));
+    }
+
+    uiDeleteAsync<T>(path:string,data:any=null):Promise<T|null>
+    {
+        return this.uiContext<T>(()=>(this.deleteAsync(path,data) as Promise<T>));
+    }
+    
+    uiPostFormAsync<T>(path:string,formData:any):Promise<T|null>
+    {
+        return this.uiContext<T>(()=>(this.postFormAsync(path,formData) as Promise<T>));
+    }
+
+    private async uiContext<T>(request:()=>Promise<T>):Promise<T|null>
+    {
+        try{
+            return await request();
+        }catch(ex){
+            Log.error('http error',ex);
+            return null;
+        }
     }
 }
