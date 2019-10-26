2fbc4357c8d71e39e6b875a1d9b96467ce66a7bc
Scott Vance <logic@newbaselogic.com>
2019-10-26 18:15:44 -0400

    - Added SlidePanel

---
+++ SlidePanel.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { mergeClassNames } from './utilTs';
+import './SlidePanel.scss';
+
+interface SlidePanelProps
+{
+    index:number;
+    setIndex?:(index:number)=>void;
+    className?:string;
+    defaultClassName?:string;
+    direction?:'horizontal'|'vertical';
+    children:any;
+}
+
+export default function SlidePanel({
+    index,
+    setIndex,
+    className,
+    defaultClassName='slide-panel-default',
+    direction='horizontal',
+    children
+}:SlidePanelProps)
+{
+
+    const hr=direction==='horizontal';
+
+    const [divRef,setDivRef]=useState<HTMLDivElement|null>(null);
+
+    const pos=(divRef?(hr?divRef.clientWidth:divRef.clientHeight):0)*index;
+
+    const cn=mergeClassNames(
+        defaultClassName+
+        (direction?(' slide-panel-'+direction):''),
+        className);
+
+    return (
+        <div className={cn} ref={r=>setDivRef(r)}>
+            <ul style={{transform:'translate'+(hr?'X':'Y')+'('+(-pos)+'px)'}}>
+                {React.Children.map(children,(slide:any,i:number)=>{
+                    return (
+                        <li key={i}>{slide}</li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
+}
