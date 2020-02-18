0c46f9e729db11bf0d29edf4b0c53ed2f307242b
Scott Vance <logic@newbaselogic.com>
2020-02-18 16:33:15 -0500

    - Added useAnimatedValues

--- CommonHooks.ts
+++ CommonHooks.ts
@@ -1,4 +1,4 @@
-import {useCallback,useEffect,useState, DependencyList} from 'react';
+import {useCallback,useEffect,useState, DependencyList, useMemo} from 'react';
 import Log from './Log';
 
 export function useAsync<T,D>(defaultValue:D,asyncCallback:()=>Promise<T>,deps:DependencyList):T|D
@@ -69,4 +69,55 @@ export function useNumberCounter(
     },[loop,stepDuration,from,to,autoReverse]);
 
     return step;
+}
+
+export interface AnimatedValue<T>
+{
+    value:T;
+    duration:number;
+}
+
+export function useAnimatedValues<T>(values:AnimatedValue<T>[],deps:DependencyList=[]):T
+{
+    // eslint-disable-next-line
+    const _timings=useMemo(()=>values||[],deps);
+
+    const [index,setIndex]=useState(0);
+
+    useEffect(()=>{
+
+        if(_timings.length===0){
+            return;
+        }
+
+        let m=true;
+
+        const step=()=>{
+
+            if(!m){
+                return;
+            }
+
+            setIndex(i=>{
+                if(!m){
+                    return i;
+                }
+                i++;
+                if(i>=_timings.length){
+                    return i-1;
+                }
+                setTimeout(step,_timings[i].duration);
+                return i;
+            });
+        }
+
+        setTimeout(step,_timings[0].duration);
+
+        return ()=>{
+            m=false;
+        }
+
+    },[_timings]);
+
+    return index<_timings.length?_timings[index].value:(undefined as any);
 }
