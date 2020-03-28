a0946e16ffa99ac2ad791e8c41c84ec792daf428
Scott Vance <logic@newbaselogic.com>
2020-03-28 14:27:08 -0400

    - Modal now accepts a color as background

--- Modal.tsx
+++ Modal.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Animated } from 'react-native';
+import { StyleSheet, Animated, View } from 'react-native';
 import Portal from './Portal-rn';
 import { useDimensions } from './Dimensions-rn';
 import { useTween } from './Animations-rn';
@@ -17,7 +17,7 @@ interface ModalProps
     isOpen:boolean;
     closeRequested?:(isOpen:boolean)=>void;
     hideTimeout?:number;
-    bg?:BgRender|null;
+    bg?:BgRender|string|null;
     children:any;
 }
 
@@ -63,7 +63,9 @@ export default function Modal({
                 height,
                 transform:[{translateY:tween.map(0,-height)}]
             }}>
-                {bg===undefined?(defaultBgRender&&defaultBgRender()):(bg&&bg())}
+                {(typeof bg === 'string')?
+                    <View style={[styles.solidColor,{backgroundColor:bg}]}/>:
+                    (bg===undefined?(defaultBgRender&&defaultBgRender()):(bg&&bg()))}
                 {children}
             </Animated.View>
         </Portal>
@@ -74,5 +76,12 @@ export default function Modal({
 const styles=StyleSheet.create({
     root:{
         flex:1
+    },
+    solidColor:{
+        position:'absolute',
+        left:0,
+        top:0,
+        width:'100%',
+        height:'100%'
     }
 });
