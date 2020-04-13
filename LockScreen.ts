2217a457784fb8e2f1514d252ce8e47beecc4966
Scott Vance <logic@newbaselogic.com>
2020-04-13 15:34:26 -0400

    - Added progress to LockScreen

--- LockScreen.ts
+++ LockScreen.ts
@@ -1,13 +1,16 @@
 import EventEmitterEx, { useEmitter } from "../CommonJs/EventEmitterEx-rn";
-import util from "../CommonJs/util";
+import util from "./util";
 import { useState, useMemo, useContext } from "react";
 import React from "react";
 
 export interface LockHandle
 {
+    id:number;
     name:string;
     description:any;
     unlock:()=>void;
+    setProgress:(progress:number|null)=>void;
+    progress:number|null;
 }
 
 export default class LockScreen extends EventEmitterEx
@@ -15,27 +18,45 @@ export default class LockScreen extends EventEmitterEx
 
     locks:LockHandle[]=[];
 
+    private _current:LockHandle|null=null;
+
     private _lastLockName:string|null=null;
 
     public get lastLockName():string|null{
         return this._lastLockName;
     }
 
+    private _progress:number|null=null;
+    public get progress(){return this._progress}
+
     addLock(name:string,description?:any):LockHandle{
-        const self=this;
         const handle:any={name,description};
         handle.unlock=()=>{
-            if(util.removeItem(self.locks,handle)){
-                if(self.locks.length){
-                    this._lastLockName=self.locks[self.locks.length-1].name;
+            if(util.removeItem(this.locks,handle)){
+                if(this.locks.length){
+                    this._current=this.locks[this.locks.length-1];
+                    this._lastLockName=this._current?.name||null;
+                    this._progress=this._current?.progress||null;
                 }
-                self.emit('lock',self.locks);
+                this.emit('lock',this.locks);
+                this.emit('progress',this._progress);
             }
         };
+        handle.progress=null;
+        handle.setProgress=(progress:number|null)=>{
+            handle.progress=progress;
+            if(this._current===handle && progress!==this._progress){
+                this._progress=progress;
+                this.emit('progress',progress);
+            }
+        }
         this._lastLockName=name;
-        self.locks.push(handle);
+        this._current=handle;
+        this._progress=null;
+        this.locks.push(handle);
 
-        self.emit('lock',self.locks);
+        this.emit('lock',this.locks);
+        this.emit('progress',null);
 
         return handle as LockHandle;
     }
@@ -49,7 +70,7 @@ export default class LockScreen extends EventEmitterEx
 export const LockScreenContext=React.createContext<LockScreen|null>(null);
 
 export interface Lock{
-    lock:(name:string)=>void;
+    lock:(name:string)=>LockHandle;
     unlock:()=>void;
 }
 export function useLockScreen():Lock{
@@ -66,6 +87,7 @@ export function useLockScreen():Lock{
         lock:(name:string)=>{
             const h=lockScreen.addLock(name);
             handles.push(h);
+            return h;
         },
         unlock:()=>{
             const h=handles.pop();
