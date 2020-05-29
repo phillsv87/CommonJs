3f3e2b5fd278121055a5c6424dfc45e6dbd37bb5
Scott Vance <logic@newbaselogic.com>
2020-05-28 20:48:48 -0400

    - Added the Box compontent

---
+++ Box-rn.tsx
@@ -0,0 +1,42 @@
+import React, { useState, useCallback } from 'react';
+import { View, StyleSheet, ViewStyle, StyleProp, LayoutChangeEvent } from 'react-native';
+
+interface BoxProps
+{
+    style?: StyleProp<ViewStyle>;
+    boxStyle?: StyleProp<ViewStyle>;
+    children?:any;
+}
+
+export default function Box({
+    style,
+    boxStyle,
+    children
+}:BoxProps){
+
+    const [size,setSize]=useState<null|number>(null);
+
+    const onLayout=useCallback((e:LayoutChangeEvent)=>{
+        setSize(Math.min(e.nativeEvent.layout.width,e.nativeEvent.layout.height));
+    },[]);
+
+    return (
+        <View style={[styles.root,style]} onLayout={onLayout}>
+            <View style={[size===null?null:{
+                width:size,
+                height:size
+            },boxStyle]}>
+                {children}
+            </View>
+        </View>
+    )
+
+}
+
+const styles=StyleSheet.create({
+    root:{
+        flex:1,
+        justifyContent:'center',
+        alignContent:'center'
+    }
+});
