96c14c0ed30b20c3ebe4ed26491e67025f792500
Scott Vance <logic@newbaselogic.com>
2020-09-10 11:39:33 -0400

    - Added style prop to Blur

--- Blur-rn.tsx
+++ Blur-rn.tsx
@@ -1,6 +1,7 @@
 // requires @react-native-community/blur package
 import React from 'react';
 import { BlurView } from "@react-native-community/blur";
+import { StyleProp, ViewStyle } from 'react-native';
 
 export const defaultBlurAmount=10;
 
@@ -9,25 +10,27 @@ interface BlurProps
     borderRadius?:number;
     invert?:boolean;
     amount?:number;
+    style?:StyleProp<ViewStyle>;
 }
 
 export default function Blur({
     borderRadius=0,
     invert,
-    amount=defaultBlurAmount
+    amount=defaultBlurAmount,
+    style
 }:BlurProps)
 {
 
     return (
             <BlurView
-                style={{
+                style={[{
                     position: "absolute",
                     top: 0,
                     left: 0,
                     bottom: 0,
                     right: 0,
                     borderRadius
-                }}
+                },style]}
                 blurType={invert?'light':'dark'}
                 blurAmount={amount}
                 />
