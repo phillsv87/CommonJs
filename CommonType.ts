32802ab1c8f42cf128e14ca341d649c61f885851
Scott Vance <logic@newbaselogic.com>
2020-09-22 18:44:34 -0400

    - Added Rect interfaces

--- CommonType.ts
+++ CommonType.ts
@@ -2,3 +2,19 @@ export type StrDictionary<TValue> = {[key:string]:TValue}
 export type NumDictionary<TValue> = {[key:number]:TValue}
 
 export type Sides = 'top'|'left'|'right'|'bottom';
+
+export interface Rect
+{
+    x:number;
+    y:number;
+    width:number;
+    height:number;
+}
+
+export interface LayoutRect
+{
+    left:number;
+    top:number;
+    width:number;
+    height:number;
+}
