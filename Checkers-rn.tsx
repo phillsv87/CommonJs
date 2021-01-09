0dede3561019795cb6afe3a1a680f7efb8abf08d
Scott Vance <logic@newbaselogic.com>
2021-01-09 18:08:07 -0500

    - Added CheckersContainer

--- Checkers-rn.tsx
+++ Checkers-rn.tsx
@@ -1,5 +1,5 @@
-import React, { useMemo } from 'react';
-import { View } from 'react-native';
+import React, { useCallback, useMemo, useState } from 'react';
+import { LayoutChangeEvent, View, ViewProps } from 'react-native';
 
 interface CheckersProps
 {
@@ -40,4 +40,50 @@ export default function Checkers({
         }
         return colAry;
     },[cols,rows,size,colorA,colorB]) as any;
+}
+
+interface CheckersContainerProps extends ViewProps
+{
+    size?:number;
+    colorA?:string|null;
+    colorB?:string|null;
+    children?:any;
+}
+
+export function CheckersContainer({
+    size=6,
+    colorA,
+    colorB,
+    children,
+    onLayout,
+    ...props
+}:CheckersContainerProps)
+{
+
+    const [layout,setLayout]=useState({w:0,h:0});
+    const cols=Math.ceil(layout.w/size);
+    const rows=Math.ceil(layout.h/size);
+
+    const _onLayout=useCallback((e:LayoutChangeEvent)=>{
+
+        setLayout({w:e.nativeEvent.layout.width,h:e.nativeEvent.layout.height});
+
+        if(onLayout){
+            onLayout(e);
+        }
+    },[onLayout])
+
+    return (
+        <View {...props} onLayout={_onLayout}>
+            <View style={{
+                position:'absolute',
+                width:layout.w,
+                height:layout.h,
+                overflow:'hidden'
+            }}>
+                <Checkers cols={cols} rows={rows} size={size} colorA={colorA} colorB={colorB} />
+            </View>
+            {children}
+        </View>
+    )
 }
