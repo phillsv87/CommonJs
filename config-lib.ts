78900c0634e83c3d0107b79f72b79bc82deea054
Scott Vance <logic@newbaselogic.com>
2021-10-29 11:09:54 -0400

    - Added config-lib

---
+++ config-lib.ts
@@ -0,0 +1,69 @@
+import * as fs from 'expo-file-system';
+import * as path from 'path';
+
+export async function getEnvAsync<T extends string>(fileName:string, dev:T, notDev:T, allOptions:T[]):Promise<T>
+{
+    try{
+        const envPath=path.isAbsolute(fileName)?fileName:path.join(fs.documentDirectory||'',fileName);
+        const stat=await fs.getInfoAsync(envPath);
+        if(stat.exists && !stat.isDirectory){
+            const env=(await fs.readAsStringAsync(envPath)).trim().toLowerCase();
+            if(allOptions.includes(env as T)){
+                return env as T;
+            }
+        }
+    }catch{}
+
+    return __DEV__?dev:notDev;
+
+
+}
+
+export async function setEnvAsync<T extends string>(fileName:string,env:T|null):Promise<boolean>
+{
+    try{
+        const envPath=path.isAbsolute(fileName)?fileName:path.join(fs.documentDirectory||'',fileName);
+        if(env===null){
+            await fs.deleteAsync(envPath);
+        }else{
+            await fs.writeAsStringAsync(envPath,env);
+        }
+        return true;
+    }catch(ex:any){
+        console.error('Unable to write env to file',ex);
+        return false;
+    }
+}
+
+export async function getConfigOverridesAsync<T>(fileName:string):Promise<Partial<T>>
+{
+    try{
+        const envPath=path.isAbsolute(fileName)?fileName:path.join(fs.documentDirectory||'',fileName);
+        const stat=await fs.getInfoAsync(envPath);
+        if(stat.exists && !stat.isDirectory){
+            const config=(await fs.readAsStringAsync(envPath)).trim();
+            if(config){
+                return JSON.parse(config);
+            }
+        }
+    }catch{}
+
+    return {}
+}
+
+export async function setConfigOverridesAsync<T>(fileName:string,overrides:Partial<T>|null):Promise<boolean>
+{
+    try{
+        const configPath=path.isAbsolute(fileName)?fileName:path.join(fs.documentDirectory||'',fileName);
+        if(overrides===null){
+            await fs.deleteAsync(configPath);
+        }else{
+            console.log('config JSON',JSON.stringify(overrides))
+            await fs.writeAsStringAsync(configPath,JSON.stringify(overrides),{encoding:fs.EncodingType.UTF8});
+        }
+        return true;
+    }catch(ex:any){
+        console.error('Unable to write config overrides to file',ex);
+        return false;
+    }
+}
