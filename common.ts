3d0dfb5ea942a24580b2e30645d8e535523c97cf
Scott Vance <logic@newbaselogic.com>
2022-01-25 14:38:53 -0500

    - Added deepCompare

--- common.ts
+++ common.ts
@@ -186,6 +186,65 @@ export function aryRandomize<T>(ary:T[]):T[]
     return newAry;
 }
 
+
+export type KeyComparer=(key:string,depth:number,a:any,b:any,state:any)=>boolean|undefined;
+
+export function deepCompare(a:any, b:any, keyComparer?:KeyComparer, keyComparerState?:any, maxDepth=200, depth=0):boolean
+{
+    if(maxDepth<0){
+        throw new Error('deepCompare max depth reached');
+    }
+    maxDepth--;
+    const type=typeof a;
+    if(type !== (typeof b)){
+        return false
+    }
+
+    if(type !== 'object'){
+        return a===b;
+    }
+
+    if(Array.isArray(a)){
+        if(a.length!==b.length){
+            return false;
+        }
+        for(let i=0;i<a.length;i++){
+            if(!deepCompare(a[i],b[i],keyComparer,keyComparerState,maxDepth,depth+1))
+            {
+                return false;
+            }
+        }
+    }else{
+        let ac=0;
+        for(const e in a){
+            ac++;
+            if(keyComparer){
+                const r=keyComparer(e,depth,a,b,keyComparerState);
+                if(r===false){
+                    return false;
+                }else if(r===true){
+                    continue;
+                }
+            }
+            if(!deepCompare(a[e],b[e],keyComparer,keyComparerState,maxDepth,depth+1))
+            {
+                return false;
+            }
+        }
+        let dc=0;
+        for(const e in b){
+            dc++;
+        }
+        if(ac!==dc){// ;)
+            return false;
+        }
+    }
+
+    return true;
+
+
+}
+
 export function areShallowEqual(a:any, b:any, shouldTestKey?:(key:string)=>boolean):boolean
 {
     if(!a && !b)
